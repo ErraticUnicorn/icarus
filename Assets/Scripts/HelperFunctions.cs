9e9eab1e5238b9480335c6b5d89881b51531817e
David Wert <david.j.wert@gmail.com>
2019-12-01 17:11:27 -0600

    begins making grid

---
+++ Assets/Scripts/HelperFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperFunctions : MonoBehaviour {
+
+	public static int width = 10;
+	public static int height = 20;
+	public static Transform[,] grid = new Transform[width, height];
+
+	public static Vector2 roundVector2(Vector2 v) {
+		return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+	}
+
+	public static bool insideBorder(Vector2 pos) {
+		return ((int)pos.x >= 0 && (int)pos.x < width && (int)pos.y >= 0);
+	}
+
+}
